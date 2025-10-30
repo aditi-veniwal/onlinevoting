c365d02738aee3cc7f21054f99f33a1ed6ebe31d
Aditi Veniwal <aditiveniwal254@gmail.com>
2025-10-30 22:28:12 +0530

    s

--- blockchain/hardhat.config.js
+++ blockchain/hardhat.config.js
@@ -1,9 +1,12 @@
+require("dotenv").config();
 require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: "0.8.28",
   networks: {
-    hardhat: {}, // local blockchain
-  },
+    sepolia: {
+      url: process.env.RPC_URL,
+      accounts: [ process.env.PRIVATE_KEY ] // ensure it begins with 0x
+    }
+  }
 };
